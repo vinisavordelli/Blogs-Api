672e4cf3eba02c50f6ee6e7b264eef432370dc02
Vinicius <vinicius.savordelli@gmail.com>
2022-04-12 18:39:11 -0300

    feat: BlogPost model

---
+++ models/BlogPost.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+      title: DataTypes.STRING,
+      content: DataTypes.STRING,
+      userId: { type: DataTypes.INTEGER, foreignKey: true },
+      published: DataTypes.DATE,
+      updated: DataTypes.DATE },
+    { tableName: 'BlogPosts', createdAt: 'published', updatedAt: 'updated',
+    });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, {
+      foreignKey: 'userId',
+      as: 'user',
+    });
+  };
+
+  return BlogPost;
+};

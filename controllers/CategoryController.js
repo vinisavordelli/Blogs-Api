20618254c65333d6b6374453aaf86706cc709326
Vinicius <vinicius.savordelli@gmail.com>
2022-04-14 14:26:32 -0300

    feat: category controller

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -1,6 +1,17 @@
 const { StatusCodes } = require('http-status-codes');
 const CategoryService = require('../services/CategoryService');
 
+const createCategory = async (req, res, next) => {
+  const { name } = req.body;
+  try {
+    const user = await CategoryService.createCategory({ name });
+    return res.status(StatusCodes.OK).json(user);
+  } catch (err) {
+    console.log(err);
+    next({ statusCode: StatusCodes.INTERNAL_SERVER_ERROR, message: 'Internal server error' });
+  }
+};
+
 const findAll = async (_req, res, next) => {
   try {
     const users = await CategoryService.findAll();
@@ -13,4 +24,5 @@ const findAll = async (_req, res, next) => {
 
 module.exports = {
   findAll,
+  createCategory,
 };
